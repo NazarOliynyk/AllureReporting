822e5a46032e1ef9762b29f0991153ca3f0c756c
Nazar <nazar_lw@ukr.net>
2020-06-16 21:59:03 +0300

    add allure.properties modify pom.xml

--- src/test/java/allureReports/AllureListener.java
+++ src/test/java/allureReports/AllureListener.java
@@ -14,18 +14,18 @@ public class AllureListener implements ITestListener {
 	private static String getTestMethodName(ITestResult iTestResult) {
 		return iTestResult.getMethod().getConstructorOrMethod().getName();
 	}
-	
+
 	@Attachment
 	public byte[] saveFailureScreenShot(WebDriver driver) {
 		return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
 	}
-	
+
 	@Attachment(value = "{0}", type = "text/plain")
 	public static String saveTextLog(String message) {
 		return message;
 	}
-		
-	
+
+
 	@Override
 	public void onStart(ITestContext iTestContext) {
 		System.out.println("I am in onStart method " + iTestContext.getName());
@@ -50,17 +50,17 @@ public class AllureListener implements ITestListener {
 	@Override
 	public void onTestFailure(ITestResult iTestResult) {
 		System.out.println("I am in onTestFailure method " + getTestMethodName(iTestResult) + " failed");
-		Object testClass = iTestResult.getInstance();
+//		Object testClass = iTestResult.getInstance();
 		WebDriver driver = BaseClass.getDriver();
 		// Allure ScreenShot and SaveTestLog
-		if (driver instanceof WebDriver) {
+		if (driver != null) {
 			System.out.println("Screenshot captured for test case:" + getTestMethodName(iTestResult));
 			saveFailureScreenShot(driver);
 		}
-		saveTextLog(getTestMethodName(iTestResult) + " failed and screenshot taken!");	
+		saveTextLog(getTestMethodName(iTestResult) + " failed and screenshot taken!");
 	}
 
-	
+
 	@Override
 	public void onTestSkipped(ITestResult iTestResult) {
 		System.out.println("I am in onTestSkipped method " + getTestMethodName(iTestResult) + " skipped");
